1801fe435606c9e6cc61c96e3ac275da92c418c9
Rye G <richard.gregory@student.swic.edu>
2024-03-28 18:56:27 -0500

    thursdayhw change pt 1

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const age = 22
-const name = "Rye"
-
-if (age >= 18) {
-    console.log ("Come in "+ name)
-}
-else {
-    console.log ("Get out")
-}
+const person = [personName= "John", personAge= 24,];
+  
+  function verifyAdulthood(person) {
+    if (personAge >= 18) {
+        return `Welcome, ${personName}!`;
+    }
+    else {
+    return `You are not old enough to enter, ${personName}.`;
+    }
+  }
+  
+  const adultMessage = verifyAdulthood(personName, personAge);
+  
+  console.log(adultMessage);
